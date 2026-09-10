38ea423f54d251cee3acb060cf9aa29a601998e1
Shagufta Nahid <shagufta.nahid@thoughtworks.com>
2026-09-10 14:20:48 +0100

    add github workflow

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ def details():
     return jsonify({
         'time': datetime.date.today().strftime("%Y-%m-%d"),
         'hostname': socket.gethostname(),
-        'message': 'You are doing a great job! Humanity is proud of you. Keep up the good work!'    
+        'message': 'You are doing a great job! Using GitOPs'    
         })
 
 
